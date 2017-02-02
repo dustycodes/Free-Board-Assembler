f908c0e0066fad0d0f777a9004ce6ed67f789c9e
Dusty Argyle <u0770958@utah.edu>
2017-02-02 15:48:42 -0700

    Adding all the things

---
+++ TeamFreeBoardAssembler/Instruction.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamFreeBoardAssembler
+{
+    class Instruction
+    {
+        public String op_code;              //bits 15-12
+        public String r_dest;
+        public String op_code_ext;
+        public String r_src;
+        public bool is_immediate;           //if it is immediate, 7-0 are immediate value or op_code_ext + r_src
+        public String binary;
+        Dictionary<String, String> registers = new Dictionary<string, string>();
+
+        public Instruction(String op_code, String op_code_ext)
+        {
+            this.op_code = op_code;
+            this.r_dest = "";
+            this.op_code_ext = op_code_ext;
+            this.r_src = "";
+
+            if (op_code_ext.Length == 0)
+                this.is_immediate = true;
+            else
+                this.is_immediate = false;
+
+            registers["$accel"] = "0000";  //IMU
+            registers["$efb"] = "0001";  
+            registers["$top_row"] = "0010";  
+            registers["$guy_location"] = "0011";
+            registers["$endfb"] = "0100";
+            registers["$loc"] = "0101";
+            registers["$newloc"] = "0110";
+            registers["$temp"] = "0111";
+            registers["$right_boundary"] = "1000";
+            registers["$left_boundary"] = "1001";
+            registers["$r10"] = "1010";
+            registers["$r11"] = "1011";
+            registers["$r12"] = "1100";
+            registers["$r13"] = "1101";
+            registers["$iter"] = "1110";
+            registers["$score"] = "1111";
+
+            this.binary = "";
+        }
+
+        public bool IsImmediate()
+        {
+            return is_immediate;
+        }
+
+        public String GetBinary(String r_src, String r_dest, int i)
+        {
+            if (!registers.ContainsKey(r_dest))
+                System.Windows.MessageBox.Show("Register " + r_dest + "does not exist, on line " + i);
+
+            if (is_immediate)
+                this.binary = this.op_code + registers[r_dest] + Immediate_To_Binary(r_src, i);
+            else
+            {
+                if (!registers.ContainsKey(r_src))
+                    System.Windows.MessageBox.Show("Register " + r_src + "does not exist, on line " + i);
+                this.binary = this.op_code + registers[r_dest] + op_code_ext + registers[r_src];
+            }
+
+            if (this.binary.Length != 16)
+                System.Windows.MessageBox.Show("Bad instruction - binary is larger than 16 bits, on line " + i);
+            return this.binary;
+        }
+
+        public bool IsBranch()
+        {
+            if (this.op_code == "0100" && this.op_code_ext == "1100")
+                return true;
+            else
+                return false;
+        }
+
+        private string Immediate_To_Binary(string r_src, int i)
+        {
+            string immediate_bin = "";
+            int temp;
+            if(Int32.TryParse(r_src, out temp))
+            {
+                immediate_bin = Convert.ToString(temp, 2);
+                while (immediate_bin.Length < 8)
+                    immediate_bin = "0" + immediate_bin;
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("Bad immediate " + r_src + ", on line " + i);
+                return "";
+            }
+            return immediate_bin;
+        }
+
+        private string Cond_Immediate_To_Binary(string r_src, bool cond, int i)
+        {
+            string immediate_bin = "";
+            int temp;
+            if (Int32.TryParse(r_src, out temp))
+            {
+                if (temp < 0)
+                {
+                    immediate_bin = Convert.ToString(temp, 2);
+                    int remaining = 8;
+                    if (cond)
+                        remaining = 4;
+                    string b = immediate_bin.Substring(immediate_bin.Length - remaining + 1);
+                    b = "1" + b;
+                    return b;
+                }
+                else
+                {
+                    immediate_bin = Convert.ToString(temp, 2);
+
+                    int remaining = 8;
+                    if (cond)
+                        remaining = 4;
+                    while (immediate_bin.Length < remaining)
+                        immediate_bin = "0" + immediate_bin;
+                }
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("Bad immediate " + r_src + ", on line " + i);
+                return "";
+            }
+            return immediate_bin;
+        }
+
+        internal string Branch(string cond, string location, int i)
+        {
+            this.binary = this.op_code + Cond_Immediate_To_Binary(cond, true, i) + Cond_Immediate_To_Binary(location, false, i);
+
+            if (this.binary.Length != 16)
+                System.Windows.MessageBox.Show("Bad branch instruction - binary is not 16 bits, on line " + i + " bcond " + cond + ", " + location );
+            return this.binary;
+        }
+
+        internal string Jump(string cond, string reg, int i)
+        {
+            if (registers.ContainsKey(reg))
+                this.binary = this.op_code + Cond_Immediate_To_Binary(cond, true, i) + this.op_code_ext + registers[reg];
+            else
+                System.Windows.MessageBox.Show("Bad register in jump instruction line number " + i);
+
+            return this.binary;
+        }
+    }
+}
---
+++ TeamFreeBoardAssembler/MainWindow.xaml.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TeamFreeBoardAssembler
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Dictionary<string, Instruction> instructions = new Dictionary<string, Instruction>();
+        List<string> one_arg = new List<string>() { "di", "ei", "retx", "wait", "scond", "bcond", "jcond" };
+        Dictionary<string, string> variables = new Dictionary<string, string>();
+        Dictionary<string, int> labels = new Dictionary<string, int>();
+        int replace_lines = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeInstructions();
+            checkBox.IsChecked = true;
+            AssemblyCode.Text = "";
+            AssemblyCode.AcceptsReturn = true;
+            AssemblyCode.AcceptsTab = true;
+            BinaryCode.Text = "";
+            BinaryCode.AcceptsReturn = true;
+            BinaryCode.AcceptsTab = true;
+        }
+
+        private void InitializeInstructions()
+        {
+            instructions["add"] = new Instruction("0000", "0101");
+            instructions["addi"] = new Instruction("0101", "");
+            instructions["addu"] = new Instruction("0000", "0110");
+            instructions["addui"] = new Instruction("0110", "");
+            instructions["mult"] = new Instruction("0000", "1110");
+            instructions["multi"] = new Instruction("1110", "");
+            instructions["sub"] = new Instruction("0000", "1001");
+            instructions["subi"] = new Instruction("1001", "");
+            instructions["cmp"] = new Instruction("0000", "1011");
+            instructions["cmpi"] = new Instruction("1011", "");
+            instructions["cmpu"] = new Instruction("1000", "1000");
+            instructions["cmpui"] = new Instruction("1000", "");
+            instructions["and"] = new Instruction("0000", "0001");
+            instructions["andi"] = new Instruction("0001", "");
+            instructions["or"] = new Instruction("0000", "0010");
+            instructions["ori"] = new Instruction("0010", "");
+            instructions["xor"] = new Instruction("0000", "0011");
+            instructions["xori"] = new Instruction("0011", "");
+            instructions["mov"] = new Instruction("0000", "1101");
+            instructions["movi"] = new Instruction("1101", "");
+            //instructions["lui"] = new Instruction("1111", "");
+            instructions["load"] = new Instruction("0100", "0000");
+            instructions["store"] = new Instruction("0100", "0100");
+            instructions["bcond"] = new Instruction("1100", ""); //? //if 0 in cond absolute, 1 overflow, 2 input 1 > input2, 3 input1 == input2 >>>> 11-8 bits 
+            instructions["jcond"] = new Instruction("0100", "1100");
+            instructions["accel"] = new Instruction("0000", "0000");
+            //instructions["jal"] = new Instruction("0100", "1000");
+            instructions["wait"] = new Instruction("0000", "0000");
+        }
+        static string current = "";
+        static int b = 0;
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            label.Content = "";
+            variables = new Dictionary<string, string>();
+            labels = new Dictionary<string, int>();
+            pc = 0;
+            replace_lines = 1;
+            try
+            {
+                int b = 0;
+                string current = "";
+                for (int i = 0; i < AssemblyCode.LineCount; i++)
+                {
+                    String line = AssemblyCode.GetLineText(i);
+                    string[] temp = line.Split('#');
+                    line = temp[0];
+                    current = line;
+                    b = i;
+                    line = line.Replace("\n", "");
+                    line = line.Replace("\t", "");
+                    line = line.Replace("\r", "");
+                    line = line.Replace(",", "");
+                    if (line == "" || line == " ")
+                        continue;
+                    IsInstruction(line, i);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("line number: " + b + "\ninstruction " + current + "\nmessage: " + ex.Message);
+                MessageBox.Show(ex.ToString());
+            }
+            pc = 0;
+            try {
+                BinaryCode.Text = "";
+                for (int i = 0; i < AssemblyCode.LineCount; i++)
+                {
+                    String line = AssemblyCode.GetLineText(i);
+                    if (i > 80)
+                        Console.WriteLine("dkjf");
+                    string[] temp = line.Split('#');
+                    line = temp[0];
+                    current = line;
+                    b = i;
+                    line = line.Replace("\n", "");
+                    line = line.Replace("\t", "");
+                    line = line.Replace("\r", "");
+                    line = line.Replace(",", "");
+                    if (line == "" || line == " ")
+                        continue;
+                    string bin = ProcessLine(line, i+1);
+                    if (bin != "") { 
+                        //BinaryCode.Text += bin + "\n";
+                        string real_bin = "";
+                        int count = 0;
+                        if (checkBox.IsChecked == false)
+                        {
+                            foreach (char c in bin)
+                            {
+                                if (count == 4)
+                                {
+                                    real_bin += "_";
+                                    count = 0;
+                                }
+                                real_bin += c;
+                                count++;
+                            }
+                        }
+                        string retbin = real_bin;
+                        
+                        //convert to hex
+                        if (checkBox.IsChecked == true) {
+                            retbin = "";
+                            string but = "";
+                            retbin = "";
+                            foreach (char d in bin)
+                            {
+                                but += d;
+                                if(but.Length == 4)
+                                {
+                                    retbin += Convert.ToInt32(but, 2).ToString("X");
+                                    but = "";
+                                }
+                            }
+                        }
+                        label.Content = "Replace lines 1 through \n" + replace_lines + " in the memory file";
+                        replace_lines++;
+                        BinaryCode.Text += retbin + "\n";
+                    }
+                }
+            }catch (Exception ex)
+            {
+                MessageBox.Show("line number: " + b + "\ninstruction " + current + "\nmessage: " + ex.Message);
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        private void IsInstruction(string line, int i)
+        {
+            string[] words = line.Split(' ');
+            List<string> b = new List<string>();
+            foreach (string s in words)
+            {
+                if (s != "" && s != " ")
+                {
+                    b.Add(s);
+                }
+            }
+            //pc++;
+
+            if (b.Count < 1)
+                return;
+
+            string mnemonic = b[0];
+            if (mnemonic.ToLower() == "bcond")
+            {
+                pc++;
+            }
+            else if (mnemonic.ToLower() == "jcond")
+            {
+                pc++;
+            }
+            else if (instructions.ContainsKey(mnemonic))
+            {
+                pc++;
+            }
+            else if (b[0][0] == '$')
+            {
+                string[] d = line.Split('#');
+                line = d[0];
+                string[] value = line.Split('=');
+                variables[value[0]] = value[1];
+            }
+            else if (b[0][0] == '#')
+            {
+                return;
+            }
+            else if (words[0][0] == ':')
+            {
+                words = words[0].Split(':');
+                string lbl = words[1];
+                this.labels.Add(lbl, pc);
+                this.variables.Add("$" + lbl, pc.ToString());
+            }
+            else
+            {
+                MessageBox.Show("There is no instruction for " + line);
+            }
+            return;
+        }
+
+        static int pc = 0;
+
+        private string ProcessLine(string line, int i)
+        {
+            string [] words = line.Split(' ');
+            List<string> b = new List<string>();
+            foreach (string s in words)
+            {
+                if(s != "" && s != " ")
+                {
+                    b.Add(s);
+                }
+            }
+            //pc++;
+
+            if (b.Count < 1)
+                return "";
+            
+            string mnemonic = b[0];
+            if(mnemonic.ToLower() == "bcond")
+            {
+                pc++;
+                if(variables.ContainsKey(b[1]) && variables.ContainsKey(b[2]))
+                    return instructions[mnemonic].Branch(variables[b[1]], variables[b[2]], i);
+                else if (variables.ContainsKey(b[1]) && !variables.ContainsKey(b[2]))
+                    return instructions[mnemonic].Branch(variables[b[1]], b[2], i);
+                else if (!variables.ContainsKey(b[1]) && variables.ContainsKey(b[2]))
+                    return instructions[mnemonic].Branch(b[1], variables[b[2]], i);
+                else
+                    return instructions[mnemonic].Branch(b[1], b[2], i);
+            }
+            if (mnemonic.ToLower() == "jcond")
+            {
+                pc++;
+                if (variables.ContainsKey(b[1]))
+                    return instructions[mnemonic].Jump(variables[b[1]], b[2], i);
+                else
+                    return instructions[mnemonic].Jump(b[1], b[2], i);
+            }
+            else if (instructions.ContainsKey(mnemonic))
+            {
+                pc++;
+                if (variables.ContainsKey(b[1]))
+                    return instructions[mnemonic].GetBinary(variables[b[1]], b[2], i);
+                else
+                    return instructions[mnemonic].GetBinary(b[1], b[2], i);
+            }
+            else if (b[0][0] == '$')
+            {
+                /*string[] d = line.Split('#');
+                line = d[0];
+                string[] value = line.Split('=');
+                variables[value[0]] = value[1];*/
+                return "";
+            }
+            else if (b[0][0] == '#')
+            {
+                return "";
+            }
+            else if (words[0][0] == ':')
+            {
+                /*words = words[0].Split(':');
+                string lbl = words[1];
+                this.labels.Add(lbl, pc);
+                this.variables.Add("$" + lbl, pc.ToString());*/
+                return "";
+            }
+            else
+            {
+                MessageBox.Show("There is no instruction for " + line);
+            }
+            return "";
+        }
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (checkBox.IsChecked == true)
+                checkBox.Content = "hex";
+            else
+                checkBox.Content = "binary";
+        }
+
+        private void AssemblyCode_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
